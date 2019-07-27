dc5a0fa1c60eb898a66cc863bf48c5a423fe1b30
Matthijs Vos <vos1996@gmail.com>
2019-07-27 18:33:43 +0200

    rubocop

--- app/mailers/mail_smtp_mailer.rb
+++ app/mailers/mail_smtp_mailer.rb
@@ -2,11 +2,13 @@ class MailSMTPMailer < ApplicationMailer
   def enabled_email(mail_alias, password)
     @mail_alias = mail_alias
     @password = password
-    mail cc: 'mailbeheer@csvalpha.nl', bcc: mail_alias.mail_addresses, subject: "SMTP account voor #{mail_alias.email} aangemaakt"
+    mail cc: 'mailbeheer@csvalpha.nl', bcc: mail_alias.mail_addresses,
+         subject: "SMTP account voor #{mail_alias.email} aangemaakt"
   end
 
   def disabled_email(mail_alias)
     @mail_alias = mail_alias
-    mail cc: 'mailbeheer@csvalpha.nl', bcc: mail_alias.mail_addresses, subject: "SMTP account voor #{mail_alias.email} opgeheven"
+    mail cc: 'mailbeheer@csvalpha.nl', bcc: mail_alias.mail_addresses,
+         subject: "SMTP account voor #{mail_alias.email} opgeheven"
   end
 end
