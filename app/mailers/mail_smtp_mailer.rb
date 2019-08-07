e64fc8c5a4c3a230348264c788512e914e82a53a
Matthijsy <vos1996@gmail.com>
2019-08-07 09:42:46 +0200

    Add to address for SMTP mail (#62)
    * Mailgun needs a to address
    
    * Fix spec

--- app/mailers/mail_smtp_mailer.rb
+++ app/mailers/mail_smtp_mailer.rb
@@ -2,13 +2,13 @@ class MailSMTPMailer < ApplicationMailer
   def enabled_email(mail_alias, password)
     @mail_alias = mail_alias
     @password = password
-    mail cc: 'mailbeheer@csvalpha.nl', bcc: mail_alias.mail_addresses,
+    mail to: 'mailbeheer@csvalpha.nl', bcc: mail_alias.mail_addresses,
          subject: "SMTP account voor #{mail_alias.email} aangemaakt"
   end
 
   def disabled_email(mail_alias)
     @mail_alias = mail_alias
-    mail cc: 'mailbeheer@csvalpha.nl', bcc: mail_alias.mail_addresses,
+    mail to: 'mailbeheer@csvalpha.nl', bcc: mail_alias.mail_addresses,
          subject: "SMTP account voor #{mail_alias.email} opgeheven"
   end
 end
