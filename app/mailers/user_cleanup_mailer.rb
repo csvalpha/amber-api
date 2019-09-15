037cb1191b406cef7d63d24b6c3ed624b156f35f
matthijs <vos1996@gmail.com>
2019-09-15 20:43:29 +0200

    Improve layout

--- app/mailers/user_cleanup_mailer.rb
+++ app/mailers/user_cleanup_mailer.rb
@@ -4,7 +4,7 @@ class UserCleanupMailer < ApplicationMailer
     @removed_users = removed_users
     subject = '[Belangrijk!] Er gaan gebruikers worden verwijderd van de website'
     if @will_remove_users.empty?
-      subject = 'Er zijn gebruikers verwijderd'
+      subject = 'Er zijn gebruikers verwijderd!'
     end
 
     mail to: 'ict@csvalpha.nl;bestuur@csvalpha.nl', subject: subject
