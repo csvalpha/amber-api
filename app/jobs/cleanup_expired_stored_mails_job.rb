1123c8794c2d0c265f47e1a0afb2ecf3a7d939ca
Casper Smits <casper.smits@nedap.com>
2019-10-23 15:21:33 +0200

    Remove no-actions text, remove unnecessary guard clause

--- app/jobs/cleanup_expired_stored_mails_job.rb
+++ app/jobs/cleanup_expired_stored_mails_job.rb
@@ -2,28 +2,26 @@ class CleanupExpiredStoredMailsJob < ApplicationJob
   queue_as :default
 
   def perform
-    # Really destroy already moderated emails older than two weeks
+    # Really destroy already moderated emails older than four weeks
     deleted_mails = StoredMail.only_deleted.where('created_at <= ?', 4.weeks.ago)
     deleted_mails_count = deleted_mails.count
-    deleted_mails.each(&:really_destroy!) if deleted_mails_count.positive?
+    deleted_mails.each(&:really_destroy!)
 
     # Delete all expired (ignored) emails after the 3-day threshold imposed by Mailgun
     expired_mails = StoredMail.where('created_at <= ?', 4.days.ago)
     expired_mails_count = expired_mails.count
-    expired_mails.each(&:destroy) if expired_mails.any?
+    expired_mails.each(&:destroy)
 
     inform_slack(deleted_mails_count, expired_mails_count)
   end
 
   def inform_slack(deleted_count, expired_count)
-    if deleted_count.positive? && expired_count.positive?
-      SlackMessageJob.perform_later(
-        "Email cleanup finished. #{deleted_count} expired emails really destroyed; "\
-        "#{expired_count} ignored (not-moderated) emails soft-deleted",
-        channel: '#monitoring'
-      )
-    else
-      SlackMessageJob.perform_later('No email cleanup needed', channel: '#monitoring')
-    end
+    return unless deleted_count.positive? || expired_count.positive?
+
+    SlackMessageJob.perform_later(
+      "Email cleanup finished. #{deleted_count} expired #{'emails'.pluralize(deleted_count)} really destroyed; "\
+      "#{expired_count} ignored (not-moderated) #{'emails'.pluralize(expired_count)} soft-deleted",
+      channel: '#monitoring'
+    )
   end
 end
