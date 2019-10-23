83920b9b869eea2d9666ee692a2af2d158cf5a81
matthijs <vos1996@gmail.com>
2019-10-23 21:17:35 +0200

    Rubocop

--- app/mailers/user_cleanup_mailer.rb
+++ app/mailers/user_cleanup_mailer.rb
@@ -2,7 +2,8 @@ class UserCleanupMailer < ApplicationMailer
   def cleanup_email(will_archive_users, archived_users)
     @will_archive_users = will_archive_users
     @archived_users = archived_users
-    subject = "[Belangrijk!] #{@will_archive_users} Alpha accounts staan op het punt gearchiveerd te worden"
+    subject = "[Belangrijk!] #{@will_archive_users} Alpha accounts"\
+              ' staan op het punt gearchiveerd te worden'
     subject = 'Er zijn gebruikers gearchiveerd!' if @will_archive_users.empty?
 
     mail to: 'ict@csvalpha.nl;bestuur@csvalpha.nl', subject: subject
