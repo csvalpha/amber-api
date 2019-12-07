920812a82889b070f237435e3e0346ceb5aea685
matthijs <vos1996@gmail.com>
2019-12-07 21:33:30 +0100

    Update dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'simplecov'
 
 SimpleCov.start 'rails' do
   add_filter 'app/jobs/sentry_job.rb'
-  add_filter 'lib/capistrano/slack.rb'
 
   minimum_coverage 95
   minimum_coverage_by_file 95
