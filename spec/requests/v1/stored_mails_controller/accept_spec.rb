298e1652338a227d1e75446d1aacd7293a9e9fc2
matthijs <vos1996@gmail.com>
2019-12-07 20:44:08 +0100

    Add additional test case for accept mail

--- spec/requests/v1/stored_mails_controller/accept_spec.rb
+++ spec/requests/v1/stored_mails_controller/accept_spec.rb
@@ -2,11 +2,32 @@ require 'rails_helper'
 
 describe V1::StoredMailsController do
   describe 'POST /stored_mails/:id/accept', version: 1 do
+    let(:request) { perform_enqueued_jobs(except: MailForwardJob) { post(record_url) } }
+    let(:record) { FactoryBot.create(:stored_mail) }
+    let(:record_url) { "/v1/stored_mails/#{record.id}/accept" }
+    let(:record_permission) { 'stored_mail.destroy' }
+
     it_behaves_like 'a destroyable and permissible model' do
       let(:request) { perform_enqueued_jobs(only: ActionMailer::DeliveryJob) { post(record_url) } }
-      let(:record) { FactoryBot.create(:stored_mail) }
-      let(:record_url) { "/v1/stored_mails/#{record.id}/accept" }
-      let(:record_permission) { 'stored_mail.destroy' }
+    end
+
+    context 'sends accept mail' do
+      let(:accept_mail) { ActionMailer::Base.deliveries.first }
+
+      include_context 'when authenticated' do
+        let(:user) { FactoryBot.create(:user, user_permission_list: [record_permission]) }
+      end
+
+      before do
+        ActionMailer::Base.deliveries = []
+
+        perform_enqueued_jobs do
+          request
+        end
+      end
+
+      it { expect(record.sender).to include accept_mail.to.first }
+      it { expect(accept_mail.subject).to include 'Mail goedgekeurd' }
     end
   end
 end
