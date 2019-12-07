d07e956cf46b448ee05b041197cbc4096a676c53
matthijs <vos1996@gmail.com>
2019-12-07 12:28:52 +0100

    Activate all specs

--- spec/jobs/mail_receive_job_spec.rb
+++ spec/jobs/mail_receive_job_spec.rb
@@ -9,51 +9,51 @@ RSpec.describe MailReceiveJob, type: :job do
     before do
       ActionMailer::Base.deliveries = []
     end
-    #
-    # context 'when recipient mail alias does not exist' do
-    #   let(:sender) { 'alice@example.org' }
-    #   let(:ndr_mail) { ActionMailer::Base.deliveries.first }
-    #
-    #   before do
-    #     ActionMailer::Base.deliveries = []
-    #     allow(mail_fetcher_class).to receive(:new) { OpenStruct.new(sender: sender) }
-    #     perform_enqueued_jobs do
-    #       described_class.perform_now('unknown@csvalpha.nl', message_url)
-    #     end
-    #   end
-    #
-    #   it { expect(ndr_mail).not_to be nil }
-    #   it { expect(ndr_mail.to.first).to eq 'alice@example.org' }
-    #
-    #   context 'when sender is noreply' do
-    #     let(:sender) { 'noreply@csvalpha.nl' }
-    #
-    #     it { expect(ndr_mail).to be nil }
-    #   end
-    # end
-    #
-    # context 'when recipient mail alias exists' do
-    #   let(:mail_alias) { FactoryBot.create(:mail_alias, :with_user) }
-    #
-    #   before do
-    #     allow(mail_fetcher_class).to receive(:new).with(an_instance_of(String))
-    #     job.perform(mail_alias.email, message_url)
-    #   end
-    #
-    #   it { expect(mail_fetcher_class).to have_received(:new).with(message_url) }
-    # end
-    #
-    # context 'when recipient mail alias is contains uppercase' do
-    #   let(:mail_alias) { FactoryBot.create(:mail_alias, :with_user, email: 'test@csvalpha.nl') }
-    #
-    #   before do
-    #     mail_alias
-    #     allow(mail_fetcher_class).to receive(:new).with(an_instance_of(String))
-    #     job.perform('Test@csvalpha.nl', message_url)
-    #   end
-    #
-    #   it { expect(mail_fetcher_class).to have_received(:new).with(message_url) }
-    # end
+
+    context 'when recipient mail alias does not exist' do
+      let(:sender) { 'alice@example.org' }
+      let(:ndr_mail) { ActionMailer::Base.deliveries.first }
+
+      before do
+        ActionMailer::Base.deliveries = []
+        allow(mail_fetcher_class).to receive(:new) { OpenStruct.new(sender: sender) }
+        perform_enqueued_jobs do
+          described_class.perform_now('unknown@csvalpha.nl', message_url)
+        end
+      end
+
+      it { expect(ndr_mail).not_to be nil }
+      it { expect(ndr_mail.to.first).to eq 'alice@example.org' }
+
+      context 'when sender is noreply' do
+        let(:sender) { 'noreply@csvalpha.nl' }
+
+        it { expect(ndr_mail).to be nil }
+      end
+    end
+
+    context 'when recipient mail alias exists' do
+      let(:mail_alias) { FactoryBot.create(:mail_alias, :with_user) }
+
+      before do
+        allow(mail_fetcher_class).to receive(:new).with(an_instance_of(String))
+        job.perform(mail_alias.email, message_url)
+      end
+
+      it { expect(mail_fetcher_class).to have_received(:new).with(message_url) }
+    end
+
+    context 'when recipient mail alias is contains uppercase' do
+      let(:mail_alias) { FactoryBot.create(:mail_alias, :with_user, email: 'test@csvalpha.nl') }
+
+      before do
+        mail_alias
+        allow(mail_fetcher_class).to receive(:new).with(an_instance_of(String))
+        job.perform('Test@csvalpha.nl', message_url)
+      end
+
+      it { expect(mail_fetcher_class).to have_received(:new).with(message_url) }
+    end
 
     context 'when recipient is an open mail alias' do
       let(:mail_alias) { FactoryBot.create(:mail_alias, :with_user, moderation_type: :open) }
