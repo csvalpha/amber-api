eb2532b11111c27b36d3c40b5dbac49827783e94
Guido <guidojwdejong@hotmail.com>
2019-12-31 00:25:58 +0100

    Add test context for when a mail is accepted/rejected

--- spec/jobs/mail_moderation_reminder_job_spec.rb
+++ spec/jobs/mail_moderation_reminder_job_spec.rb
@@ -12,21 +12,34 @@ RSpec.describe MailModerationReminderJob, type: :job do
       end
     end
 
-    context 'when sent one day ago' do
+    context 'when expiring within 48 hours' do
       let(:stored_mail) do
         FactoryBot.create(:stored_mail, mail_alias: mail_alias, received_at: 1.day.ago)
       end
 
       it { expect(ActionMailer::Base.deliveries.count).to eq 1 }
-      it { expect{ described_class.perform_now(stored_mail) }.to have_enqueued_job(described_class) }
+
+      it do
+        expect{ described_class.perform_now(stored_mail) }.to have_enqueued_job(described_class)
+      end
     end
 
-    context 'when sent two days ago' do
+    context 'when expiring within 24 hours' do
       let(:stored_mail) do
         FactoryBot.create(:stored_mail, mail_alias: mail_alias, received_at: 2.days.ago)
       end
 
       it { expect(ActionMailer::Base.deliveries.count).to eq 1 }
+
+      it do
+        expect{ described_class.perform_now(stored_mail) }.not_to have_enqueued_job(described_class)
+      end
+    end
+
+    context 'when accepted or rejected' do
+      let(:stored_mail) {}
+
+      it { expect(ActionMailer::Base.deliveries.count).to eq 0 }
 
       it do
         expect{ described_class.perform_now(stored_mail) }.not_to have_enqueued_job(described_class)
