561724109aa2b91e31e9701aae5166f648e4e65c
Guido <guidojwdejong@hotmail.com>
2020-03-16 13:21:57 +0100

    Change job argument to id

--- app/jobs/mail_moderation_reminder_job.rb
+++ app/jobs/mail_moderation_reminder_job.rb
@@ -1,7 +1,9 @@
 class MailModerationReminderJob < ApplicationJob
   queue_as :default
 
-  def perform(stored_mail) # rubocop:disable Metrics/AbcSize
+  def perform(stored_mail_id) # rubocop:disable Metrics/AbcSize
+    stored_mail = StoredMail.find_by(id: stored_mail_id)
+
     return unless stored_mail
 
     stored_mail.mail_alias.moderators.each do |moderator|
@@ -11,6 +13,6 @@ class MailModerationReminderJob < ApplicationJob
     # Only reschedule if mail doesn't expire next day
     return unless Time.zone.now + 25.hours < stored_mail.received_at + 3.days
 
-    MailModerationReminderJob.set(wait: 24.hours).perform_later(stored_mail)
+    MailModerationReminderJob.set(wait: 24.hours).perform_later(stored_mail_id)
   end
 end
