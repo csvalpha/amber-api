2641e8ab76a178cd6c9b34cb66861b8ead513275
cpbscholten <c.p.b.scholten@student.utwente.nl>
2020-10-06 19:19:04 +0200

    fixed the tests for thread

--- app/controllers/v1/forum/threads_controller.rb
+++ app/controllers/v1/forum/threads_controller.rb
@@ -1,14 +1,14 @@
 module V1::Forum
   class ThreadsController < V1::ApplicationController
-    before_action :doorkeeper_authorize!, except: %i[index show]
+    before_action :doorkeeper_authorize!
     before_action :set_model, only: %i[mark_read]
 
     def mark_read
-      @last = @model.posts.last
-      @thread = Forum::ReadThread.where(thread: @model, user: current_user).first_or_create! do |t|
-        t.post = @last
+      last = @model.posts.last
+      thread = Forum::ReadThread.where(thread: @model, user: current_user).first_or_create! do |t|
+        t.post = last
       end
-      @thread.update(post: @last)
+      thread.update(post: last)
     end
   end
 end
