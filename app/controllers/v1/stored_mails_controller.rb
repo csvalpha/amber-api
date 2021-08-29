0f0eea3a51f21e4800954969f7a7ffd5e0697127
wilco375 <wilcovanbeijnum@gmail.com>
2021-08-29 18:23:43 +0200

    Fix redeliver loop

--- app/controllers/v1/stored_mails_controller.rb
+++ app/controllers/v1/stored_mails_controller.rb
@@ -8,7 +8,14 @@ class V1::StoredMailsController < V1::ApplicationController
       MailForwardJob.perform_later(@model.mail_alias, @model.message_url)
     else
       # :nocov:
-      MailImprovmxForwardJob.perform_later(@model)
+      to_addresses = @model.mail_alias.mail_addresses
+      to_addresses.each { |to_address|
+        mail = @model.inbound_email.mail.clone
+        mail.to = to_address
+        mail.cc = nil
+        mail.bcc = nil
+        MailImprovmxForwardJob.perform_later(mail)
+      }
       # :nocov:
     end
     MailModerationMailer.accept_email(@model.sender, @model, current_user).deliver_later
--- app/jobs/mail_improvmx_forward_job.rb
+++ app/jobs/mail_improvmx_forward_job.rb
@@ -1,26 +1,18 @@
 require 'http'
-BATCH_SIZE = 10
 # TODO: Rename after mailgun has been removed, and increase test coverage
 # :nocov:
 class MailImprovmxForwardJob < ApplicationJob
   queue_as :mail_handlers
 
-  def perform(stored_mail) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  def perform(mail) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     # TODO: use default SMTP as soon as we use ImprovMX as default
     smtp = Mail::SMTP.new({ address: 'smtp.improvmx.com', port: '587',
                             user_name: Rails.application.config.x.smtp_username,
                             password: Rails.application.config.x.smtp_password })
-    mail = stored_mail.inbound_email.mail
-    mail.to = nil
 
-    to_addresses = stored_mail.mail_alias.mail_addresses
-    while to_addresses.any?
-      mail.reply_to = mail.from
-      mail.from = new_from(mail)
-      mail.bcc = to_addresses.pop(BATCH_SIZE)
-
-      smtp.deliver!(mail)
-    end
+    mail.reply_to = mail.from
+    mail.from = new_from(mail)
+    smtp.deliver!(mail)
   end
 
   private
