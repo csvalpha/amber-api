c01fa1fa3e8e839df12fc15362e83498a04fd177
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2021-10-10 15:36:54 +0200

    Remove redundant code

--- db/migrate/20210922130714_update_photo_exif_date_to_date_time.rb
+++ db/migrate/20210922130714_update_photo_exif_date_to_date_time.rb
@@ -6,13 +6,8 @@ class UpdatePhotoExifDateToDateTime < ActiveRecord::Migration[6.1]
 
     Photo.reset_column_information
 
-    # Update date_time_original for each photo to include the time
+    # Re-save each photo to update the date_time_original to include the time
     Photo.with_deleted.each do |photo|
-      next unless photo.jpeg?
-
-      exif_data = EXIFR::JPEG.new(photo.image.file.file)
-      photo.exif_date_time_original = exif_data.date_time_original
-
       # Using touch false to prevent updating the timestamps
       photo.save(touch: false)
     end
