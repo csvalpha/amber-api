89d49f91559b48e9193b2da3c517bf08f494241d
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-02-13 16:08:33 +0100

    Add uploader for book covers

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  mount_base64_uploader :cover_photo, CoverPhotoUploader
+  mount_base64_uploader :cover_photo, BookCoverPhotoUploader
 
   validates :title, presence: true
   validates :isbn, :isbn_format => { :allow_nil => true }
