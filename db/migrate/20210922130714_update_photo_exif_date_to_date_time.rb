b3893735dceb2093f9f34273aaa5203ad4fc1163
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-03-03 14:17:47 +0100

    Fix linting error

--- db/migrate/20210922130714_update_photo_exif_date_to_date_time.rb
+++ db/migrate/20210922130714_update_photo_exif_date_to_date_time.rb
@@ -7,9 +7,7 @@ class UpdatePhotoExifDateToDateTime < ActiveRecord::Migration[6.1]
     # Re-save each photo to update the date_time_original to include the time
     Photo.with_deleted.each do |photo|
       # Using touch false to prevent updating the timestamps
-      if photo.photo_album
-        photo.save(touch: false)
-      end
+      photo.save(touch: false) if photo.photo_album
     end
   end
 
