73a63c2f43dac58badc2539aa9f35a7811ed73a6
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2023-05-07 17:27:52 +0200

    Fix job name

--- spec/requests/v1/stored_mails_controller/accept_spec.rb
+++ spec/requests/v1/stored_mails_controller/accept_spec.rb
@@ -8,7 +8,7 @@ describe V1::StoredMailsController do
     let(:record_permission) { 'stored_mail.destroy' }
 
     it_behaves_like 'a destroyable and permissible model' do
-      let(:request) { perform_enqueued_jobs(only: ActionMailer::DeliveryJob) { post(record_url) } }
+      let(:request) { perform_enqueued_jobs(only: ActionMailer::MailDeliveryJob) { post(record_url) } }
     end
 
     context 'sends accept mail' do
