93a8766d041abbcf2f041decac3830c214608d28
Ellen Wittingen <ellen2000w@gmail.com>
2023-07-28 13:23:06 +0200

    minor change

--- app/resources/v1/room_advert_resource.rb
+++ app/resources/v1/room_advert_resource.rb
@@ -1,6 +1,6 @@
 class V1::RoomAdvertResource < V1::ApplicationResource
-  attributes :house_name, :contact, :location, :available_from, :description, 
-              :description_camofied, :cover_photo_url, :cover_photo, :publicly_visible
+  attributes :house_name, :contact, :location, :available_from, :description, :description_camofied, 
+             :author_name, :cover_photo_url, :cover_photo, :publicly_visible
 
   def cover_photo_url
     @model.cover_photo.url
@@ -8,6 +8,10 @@ class V1::RoomAdvertResource < V1::ApplicationResource
 
   def description_camofied
     camofy(@model['description'])
+  end
+
+  def author_name
+   @model.author.full_name
   end
 
   has_one :author, always_include_linkage_data: true
