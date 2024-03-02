c6a7ca44f1be8abb6e0d110d892caa4a22c9f290
guidojw <35309288+guidojw@users.noreply.github.com>
2024-03-02 20:14:53 +0100

    refactor: remove irrelevant test

--- spec/requests/v1/books_controller/index_spec.rb
+++ spec/requests/v1/books_controller/index_spec.rb
@@ -39,23 +39,7 @@ describe V1::BooksController do
         let(:user) { create(:user, user_permission_list: [record_permission]) }
       end
 
-      describe 'with subtitle' do
-        it_behaves_like '200 OK'
-      end
-
-      # Commented because the isbn_lookup using bol.com is currently broken due
-      # to an expired API key problem, making acquiring a cassette for this test
-      # not possible. Work will be done in another PR on rewriting the
-      # isbn_lookup to use the Google Books API.
-      # describe 'without subtitle' do
-      #   let(:request) do
-      #     VCR.use_cassette('retrieve_book_by_isbn_no_subtitle') do
-      #       get "#{record_url}?isbn=8713638034201"
-      #     end
-      #   end
-
-      #   it_behaves_like '200 OK'
-      # end
+      it_behaves_like '200 OK'
     end
   end
 end
