ba0d9ead7b6f43f3391978038dc4934d68a5023e
DrumsnChocolate <matbronk@gmail.com>
2024-10-28 23:19:05 +0100

    oke en nu is ie ook non-null :smile:

--- db/migrate/20241018155243_update_room_adverts_available_from_string_to_date.rb
+++ db/migrate/20241018155243_update_room_adverts_available_from_string_to_date.rb
@@ -2,5 +2,6 @@ class UpdateRoomAdvertsAvailableFromStringToDate < ActiveRecord::Migration[7.0]
   def change
     remove_column :room_adverts, :available_from, :string
     add_column :room_adverts, :available_from, :date
+    change_column_null :room_adverts, :available_from, false
   end
 end
