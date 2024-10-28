a2f88c45df1348e866f8421baabb2fbae71aff76
DrumsnChocolate <matbronk@gmail.com>
2024-10-28 21:15:37 +0100

    just to be sure the migration does not fail

--- db/migrate/20241018155243_update_room_adverts_available_from_string_to_date.rb
+++ db/migrate/20241018155243_update_room_adverts_available_from_string_to_date.rb
@@ -1,5 +1,8 @@
 class UpdateRoomAdvertsAvailableFromStringToDate < ActiveRecord::Migration[7.0]
   def up
+    # Clear existing data to avoid type conversion issues
+    RoomAdvert.update_all(available_from: nil)
+    # Change column type from string to date
     change_column :room_adverts, :available_from, :date
   end
 
