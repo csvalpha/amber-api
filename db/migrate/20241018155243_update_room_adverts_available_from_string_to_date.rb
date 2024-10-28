2ab796674b87bb7cbe9e501cf576c37dbe71df10
DrumsnChocolate <matbronk@gmail.com>
2024-10-28 23:13:17 +0100

    finally, figure out how to create a reversible migration

--- db/migrate/20241018155243_update_room_adverts_available_from_string_to_date.rb
+++ db/migrate/20241018155243_update_room_adverts_available_from_string_to_date.rb
@@ -1,19 +1,6 @@
 class UpdateRoomAdvertsAvailableFromStringToDate < ActiveRecord::Migration[7.0]
-  def up
-    # Clear existing data to avoid type conversion issues
-    RoomAdvert.update_all(available_from: nil)
-    # Change column type from string to date
-    change_column :room_adverts, :available_from, :date
-    # Populate the column with a default value
-    Roomadvert.where(available_from: nil).update_all(available_from: Date.today)
-    # Make the column required
-    change_column_null :room_adverts, :available_from, false
-  end
-
-  def down
-    # Allow NULL values again
-    change_column_null :room_adverts, :available_from, true
-    # Change column type back to string
-    change_column :room_adverts, :available_from, :string
+  def change
+    remove_column :room_adverts, :available_from, :string
+    add_column :room_adverts, :available_from, :date
   end
 end
