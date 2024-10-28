3efac148096a0de6acf6cba33015b9ae5d7cc9d0
DrumsnChocolate <matbronk@gmail.com>
2024-10-28 21:31:01 +0100

    even better

--- db/migrate/20241018155243_update_room_adverts_available_from_string_to_date.rb
+++ db/migrate/20241018155243_update_room_adverts_available_from_string_to_date.rb
@@ -4,9 +4,16 @@ class UpdateRoomAdvertsAvailableFromStringToDate < ActiveRecord::Migration[7.0]
     RoomAdvert.update_all(available_from: nil)
     # Change column type from string to date
     change_column :room_adverts, :available_from, :date
+    # Populate the column with a default value
+    Roomadvert.where(available_from: nil).update_all(available_from: Date.today)
+    # Make the column required
+    change_column_null :room_adverts, :available_from, false
   end
 
   def down
+    # Allow NULL values again
+    change_column_null :room_adverts, :available_from, true
+    # Change column type back to string
     change_column :room_adverts, :available_from, :string
   end
 end
