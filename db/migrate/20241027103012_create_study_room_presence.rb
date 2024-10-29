6fa60d9ac096b1a8e7034d4bffe94c286fd44ed1
lodewiges <jamestreeg@gmail.com>
2024-10-29 02:13:31 +0100

    Fixed lint

--- db/migrate/20241027103012_create_study_room_presence.rb
+++ db/migrate/20241027103012_create_study_room_presence.rb
@@ -1,15 +1,13 @@
 class CreateStudyRoomPresence < ActiveRecord::Migration[7.0]
   def change
     create_table :study_room_presences do |t|
-      t.datetime  :start_time, null: false
-      t.datetime  :end_time, null: false
-      t.text    :status, null: false
-      t.integer   :user_id, null: false
-      t.datetime  :deleted_at
-
+      t.datetime :start_time, null: false
+      t.datetime :end_time, null: false
+      t.text :status, null: false
+      t.integer :user_id, null: false
+      t.datetime :deleted_at
       t.timestamps
     end
-
   end
 
   Permission.create(name: 'study_room_presence.create')
