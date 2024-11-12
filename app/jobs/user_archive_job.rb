7217221fc3f4731d6d3e2f1990849092da830306
Jorai Geertsema <jamestreeg@gmail.com>
2024-11-12 17:22:44 +0100

    fixed lint & tests

--- app/jobs/user_archive_job.rb
+++ app/jobs/user_archive_job.rb
@@ -20,7 +20,7 @@ class UserArchiveJob < ApplicationJob
 
     User.create!(id: ARCHIVE_USER_ID,
                  username: 'archived.user',
-                 email: <%= Rails.application.config.x.ict_email %>,
+                 email: Rails.application.config.x.ict_email,
                  first_name: 'Gearchiveerde',
                  last_name: 'Gebruiker',
                  address: 'Onbekend',
