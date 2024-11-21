7553cdcc26f0a55010c099a21ef30d8ad0440eae
Jorai Geertsema <jamestreeg@gmail.com>
2024-11-21 23:39:40 +0100

    added permissions to migration

--- db/migrate/20241113091607_remove_quickpost.rb
+++ db/migrate/20241113091607_remove_quickpost.rb
@@ -1,5 +1,10 @@
 class RemoveQuickpost < ActiveRecord::Migration[7.0]
   def change
     drop_table :quickpost_messages
+    
+    Permission.find(name: 'quickpost_message.create')&.destroy
+    Permission.find(name: 'quickpost_message.read')&.destroy
+    Permission.find(name: 'quickpost_message.update')&.destroy
+    Permission.find(name: 'quickpost_message.destroy')&.destroy
   end
 end
