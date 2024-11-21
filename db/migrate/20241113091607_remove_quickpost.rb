967a67b34b88e5bdab54155f016f1d1390587676
lodewiges <131907615+lodewiges@users.noreply.github.com>
2024-11-22 00:42:20 +0100

    Updated migration

--- db/migrate/20241113091607_remove_quickpost.rb
+++ db/migrate/20241113091607_remove_quickpost.rb
@@ -2,9 +2,9 @@ class RemoveQuickpost < ActiveRecord::Migration[7.0]
   def change
     drop_table :quickpost_messages
 
-    Permission.find(name: 'quickpost_message.create')&.destroy
-    Permission.find(name: 'quickpost_message.read')&.destroy
-    Permission.find(name: 'quickpost_message.update')&.destroy
-    Permission.find(name: 'quickpost_message.destroy')&.destroy
+    Permission.find_by(name: 'quickpost_message.create')&.destroy
+    Permission.find_by(name: 'quickpost_message.read')&.destroy
+    Permission.find_by(name: 'quickpost_message.update')&.destroy
+    Permission.find_by(name: 'quickpost_message.destroy')&.destroy
   end
 end
