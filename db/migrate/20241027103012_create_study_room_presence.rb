c7ec23f459e971a537a68f4271bb619180a3e3a7
lodewiges <131907615+lodewiges@users.noreply.github.com>
2024-11-21 23:47:04 +0100

    improved migration

--- db/migrate/20241027103012_create_study_room_presence.rb
+++ db/migrate/20241027103012_create_study_room_presence.rb
@@ -9,9 +9,9 @@ class CreateStudyRoomPresence < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    Permission.create(name: 'study_room_presence.create')
-    Permission.create(name: 'study_room_presence.read')
-    Permission.create(name: 'study_room_presence.update')
-    Permission.create(name: 'study_room_presence.destroy')
+    Permission.find_or_create_by!(name: 'study_room_presence.create')
+    Permission.find_or_create_by!(name: 'study_room_presence.read')
+    Permission.find_or_create_by!(name: 'study_room_presence.update')
+    Permission.find_or_create_by!(name: 'study_room_presence.destroy')
   end
 end
