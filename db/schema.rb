40039994772448e34d30330696d3d2b6449426c7
lodewiges <131907615+lodewiges@users.noreply.github.com>
2024-11-22 00:40:03 +0100

    Fixed studyroomMigration (#462)
    * fixed studyroomMigration
    
    * added qp again
    
    * 1/2 edited migration
    
    * 2/2 fixed schema.rb
    
    * improved migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.0].define(version: 2024_10_27_103012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -484,7 +485,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_27_103012) do
     t.index ["datetime"], name: "index_quickpost_messages_on_datetime"
     t.index ["deleted_at"], name: "index_quickpost_messages_on_deleted_at"
   end
-
+  
   create_table "room_adverts", force: :cascade do |t|
     t.string "house_name", null: false
     t.string "contact", null: false
