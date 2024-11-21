b7c03f1ccd8732e81dc3c0f73c7804ba74262a36
lodewiges <131907615+lodewiges@users.noreply.github.com>
2024-11-21 23:50:19 +0100

    Fixed Lint

--- db/migrate/20241113091607_remove_quickpost.rb
+++ db/migrate/20241113091607_remove_quickpost.rb
@@ -1,7 +1,7 @@
 class RemoveQuickpost < ActiveRecord::Migration[7.0]
   def change
     drop_table :quickpost_messages
-    
+
     Permission.find(name: 'quickpost_message.create')&.destroy
     Permission.find(name: 'quickpost_message.read')&.destroy
     Permission.find(name: 'quickpost_message.update')&.destroy
