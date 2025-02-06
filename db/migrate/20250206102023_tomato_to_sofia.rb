6eb886ebc615dc6b8221434b415cec39adc2dae2
Jorai Geertsema <jamestreeg@gmail.com>
2025-02-06 12:14:16 +0100

    Fixed lint

--- db/migrate/20250206102023_tomato_to_sofia.rb
+++ db/migrate/20250206102023_tomato_to_sofia.rb
@@ -1,6 +1,6 @@
 class TomatoToSofia < ActiveRecord::Migration[7.0]
   def up
-    rename_column( :users, :allow_tomato_sharing, :allow_sofia_sharing)
+    rename_column(:users, :allow_tomato_sharing, :allow_sofia_sharing)
   end
 
   def down
