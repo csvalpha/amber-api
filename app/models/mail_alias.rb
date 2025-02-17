76082d3291a127ec47ff66c8b28f4fd5e75d2e5b
Jorai Geertsema <jamestreeg@gmail.com>
2025-02-17 12:17:10 +0100

    intial commit

--- app/models/mail_alias.rb
+++ app/models/mail_alias.rb
@@ -13,7 +13,7 @@ class MailAlias < ApplicationRecord
 
   before_validation :downcase_email
 
-  before_save :set_smtp
+  after_save :set_smtp
   before_destroy :disable_smtp
   after_commit :sync_mail_aliases
 
