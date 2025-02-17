daeccff023981e691af0bbe3826bfd7885ca8c1f
Jorai Geertsema <jamestreeg@gmail.com>
2025-02-17 14:00:51 +0100

    Fixed Lint

--- app/models/mail_alias.rb
+++ app/models/mail_alias.rb
@@ -13,8 +13,8 @@ class MailAlias < ApplicationRecord
 
   before_validation :downcase_email
 
-  after_save :set_smtp
   before_destroy :disable_smtp
+  after_save :set_smtp
   after_commit :sync_mail_aliases
 
   scope :mail_aliases_moderated_by_user, (lambda { |user|
