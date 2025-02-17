95c6602f933e5c05e91cd713de80c6dbae5cd763
Jorai Geertsema <jamestreeg@gmail.com>
2025-02-17 01:55:36 +0100

    Fixed lint

--- db/migrate/20250216233318_remove_null_option_boolean.rb
+++ db/migrate/20250216233318_remove_null_option_boolean.rb
@@ -1,17 +1,16 @@
 class RemoveNullOptionBoolean < ActiveRecord::Migration[7.0]
+  # rubocop:disable Rails/ReversibleMigration, Rails/BulkChangeTable, Metrics/AbcSize, Layout/LineLength
   def change
-    # Backfill existing NULL values with false before setting NOT NULL constraints
-    execute "UPDATE static_pages SET publicly_visible = false WHERE publicly_visible IS NULL"
-    execute "UPDATE users SET sidekiq_access = false WHERE sidekiq_access IS NULL"
-    execute "UPDATE users SET otp_required = false WHERE otp_required IS NULL"
-    execute "UPDATE users SET ifes_data_sharing_preference = false WHERE ifes_data_sharing_preference IS NULL"
-    execute "UPDATE users SET info_in_almanak = false WHERE info_in_almanak IS NULL"
-    execute "UPDATE users SET allow_tomato_sharing = false WHERE allow_tomato_sharing IS NULL"
-    execute "UPDATE mail_aliases SET smtp_enabled = false WHERE smtp_enabled IS NULL"
-    execute "UPDATE articles SET pinned = false WHERE pinned IS NULL"
-    execute "UPDATE room_adverts SET publicly_visible = false WHERE publicly_visible IS NULL"
+    execute 'UPDATE static_pages SET publicly_visible = false WHERE publicly_visible IS NULL'
+    execute 'UPDATE users SET sidekiq_access = false WHERE sidekiq_access IS NULL'
+    execute 'UPDATE users SET otp_required = false WHERE otp_required IS NULL'
+    execute 'UPDATE users SET ifes_data_sharing_preference = false WHERE ifes_data_sharing_preference IS NULL'
+    execute 'UPDATE users SET info_in_almanak = false WHERE info_in_almanak IS NULL'
+    execute 'UPDATE users SET allow_tomato_sharing = false WHERE allow_tomato_sharing IS NULL'
+    execute 'UPDATE mail_aliases SET smtp_enabled = false WHERE smtp_enabled IS NULL'
+    execute 'UPDATE articles SET pinned = false WHERE pinned IS NULL'
+    execute 'UPDATE room_adverts SET publicly_visible = false WHERE publicly_visible IS NULL'
 
-    # Now apply NOT NULL and default constraints
     change_column_default :static_pages, :publicly_visible, false
     change_column_null :static_pages, :publicly_visible, false
 
@@ -33,4 +32,5 @@ class RemoveNullOptionBoolean < ActiveRecord::Migration[7.0]
     change_column_default :room_adverts, :publicly_visible, false
     change_column_null :room_adverts, :publicly_visible, false
   end
+  # rubocop:enable Rails/ReversibleMigration, Rails/BulkChangeTable, Metrics/AbcSize, Layout/LineLength
 end
