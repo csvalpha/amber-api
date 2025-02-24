b49add565f2433121663e9a01832796420a1d8bb
Jorai Geertsema <jamestreeg@gmail.com>
2025-02-24 18:05:44 +0100

    Fix tests v2

--- spec/resources/v1/application_resource_spec.rb
+++ spec/resources/v1/application_resource_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe V1::ApplicationResource, type: :resource do
   let(:user) { create(:user, first_name: 'John', last_name: 'Doe') }
   let(:other_user) do
-    create(:user, first_name: 'Jan', last_name_prefix: 'de', last_name: 'Vries')
+    create(:user, first_name: 'John', last_name_prefix: 'de', last_name: 'Vries')
   end
   let(:context) { { user: } }
   let(:options) { { context: } }
@@ -21,18 +21,21 @@ RSpec.describe V1::ApplicationResource, type: :resource do
       context 'when searching for one record' do
         let(:filter) { { search: ['john'] } }
 
-        it { expect(filtered).to eq [user] }
+        
+        it { expect(filtered).to include user }
+        it { expect(filtered).to include other_user }
+        it { expect(filtered.size).to eq 2 }
       end
 
       context 'when searching for multiple records' do
         let(:filter) { { search: %w[john doe] } }
 
-        it { expect(filtered).to include user }
+        it { expect(filtered).to eq [user] }
         it { expect(filtered.size).to eq 1 }
       end
 
       context 'when without records' do
-        let(:filter) { { search: ['john'] } }
+        let(:filter) { { search: ['jan'] } }
         let(:filtered) { described_class.apply_filters([], filter, options) }
 
         it { expect(filtered).to eq [] }
