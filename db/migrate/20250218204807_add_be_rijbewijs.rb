545c4b4c6f31f0d703584c5d8bd75c2101095946
Jorai Geertsema <jamestreeg@gmail.com>
2025-02-25 14:57:49 +0100

    Fixed lint

--- db/migrate/20250218204807_add_be_rijbewijs.rb
+++ db/migrate/20250218204807_add_be_rijbewijs.rb
@@ -1,6 +1,8 @@
 class AddBeRijbewijs < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :trailer_drivers_license, :boolean, default: false, null: false
-    add_column :users, :setup_complete, :boolean, default: false, null: false
+    change_table :users, bulk: true do |t|
+      t.boolean :trailer_drivers_license, default: false, null: false
+      t.boolean :setup_complete, :boolean, default: false, null: false
+    end
   end
 end
