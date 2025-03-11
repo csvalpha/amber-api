6d5dbfd8cbffd1335168b042c0a0ca2f005481fc
Lodewiges <jamestreeg@gmail.com>
2025-03-11 01:07:38 +0100

    improve aplication.rb

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails'
 require 'active_model/railtie'
 require 'active_job/railtie'
@@ -8,11 +7,7 @@ require 'active_storage/engine'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_mailbox/engine'
-# require "action_text/engine"
 # require "action_view/railtie"
-# require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
