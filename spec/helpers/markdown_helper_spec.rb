003f12db748d8e70d9eb5e374fb1e2887f042b40
Lodewiges <jamestreeg@gmail.com>
2025-03-11 02:16:41 +0100

    moved camo helper tests to sha256

--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -7,62 +7,62 @@ RSpec.describe MarkdownHelper do
 
     it do
       expect(camofy("plain text \n ![alt text](http://example.org/image.jpg)")).to eq(
-        "plain text \n ![alt text](https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067)"
+        "plain text \n ![alt text](https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067)"
       )
     end
 
     it do
       expect(camofy('plain text ![](http://example.org/image.jpg)')).to eq(
-        'plain text ![](https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067)'
+        "plain text ![](https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067)"
       )
     end
 
     # rubocop:disable Layout/LineLength
     it do
       expect(camofy('![](http://example.org/image.jpg "Image title")')).to eq(
-        '![](https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067 "Image title")'
+        '![](https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067 \"Image title\")'
       )
     end
 
     it do
       expect(camofy('![](http://example.org/image.jpg =100x* "Image title")')).to eq(
-        '![](https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067 =100x* "Image title")'
+        '![](https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067 =100x* \"Image title\")'
       )
     end
 
     it do
       expect(camofy('<img src="http://example.org/image.jpg">')).to eq(
-        '<img src="https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067">'
+        '<img src=\"https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067\">'
       )
     end
 
     it do
       expect(camofy("<img src='http://example.org/image.jpg'>")).to eq(
-        "<img src='https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067'>"
+        "<img src='https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067'>"
       )
     end
 
     it do
       expect(camofy('<img src="http://example.org/image.jpg"/>')).to eq(
-        '<img src="https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067"/>'
+        '<img src=\"https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067\"/>'
       )
     end
 
     it do
       expect(camofy('<img src="http://example.org/image.jpg" />')).to eq(
-        '<img src="https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067" />'
+        '<img src=\"https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067\">'
       )
     end
 
     it do
       expect(camofy('<img src="http://example.org/image.jpg" style="somekindofstyle" >')).to eq(
-        '<img src="https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067" style="somekindofstyle" >'
+        '<img src=\"https://example.org/d87bfef52ecbfda9b5319a1d7818fd53c4d119f2b0205cb7f8538860ac357cb0/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067\" />'
       )
     end
 
     it do
       expect(camofy('<img alt="" style="somekindofstyle" src="http://example.org/image.jpg">')).to eq(
-        '<img alt="" style="somekindofstyle" src="https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067">'
+        '<img alt=\"\" style=\"somekindofstyle\" src=\"https://example.org/c7125941763fc18c9d8977ed19028ca5f9378070/687474703a2f2f6578616d706c652e6f72672f696d6167652e6a7067\">'
       )
     end
 
