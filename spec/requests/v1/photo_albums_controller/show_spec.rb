d91f32033be5baa1d4aa5d28329c0c2efbcd59cc
Lodewiges <jamestreeg@gmail.com>
2025-03-16 20:05:07 +0100

    fixed all test

--- spec/requests/v1/photo_albums_controller/show_spec.rb
+++ spec/requests/v1/photo_albums_controller/show_spec.rb
@@ -3,19 +3,26 @@ require 'rails_helper'
 describe V1::PhotoAlbumsController do
   describe 'GET /photo_albums/:id', version: 1 do
     let(:record_permission) { 'photo_album.read' }
+
     context 'when not publicly visible' do
       let(:record) { create(:photo_album) }
       let(:record_url) { "/v1/photo_albums/#{record.id}" }
+
       subject(:request) { get(record_url) }
-    
+
       context 'when not authenticated' do
         it_behaves_like '403 Forbidden'
       end
-    
+
       context 'when member' do
         include_context 'when member' do
           let(:user) { create(:user, user_permission_list: [record_permission]) }
         end
+
+        before do
+          membership
+        end
+
         it_behaves_like '200 OK'
       end
     end
@@ -23,16 +30,22 @@ describe V1::PhotoAlbumsController do
     context 'when publicly visible' do
       let(:public_record) { create(:photo_album, :public) }
       let(:public_record_url) { "/v1/photo_albums/#{public_record.id}" }
+
       subject(:request) { get(public_record_url) }
-    
+
       context 'when not authenticated' do
         it_behaves_like '200 OK'
       end
-    
+
       context 'when member' do
         include_context 'when member' do
           let(:user) { create(:user, user_permission_list: [record_permission]) }
         end
+
+        before do
+          membership
+        end
+
         it_behaves_like '200 OK'
       end
     end
--- spec/requests/v1/photos_controller/get_related_resources_spec.rb
+++ spec/requests/v1/photos_controller/get_related_resources_spec.rb
@@ -4,7 +4,7 @@ describe V1::PhotosController do
   describe 'GET /photo-albums/:id/photos', version: 1 do
     let(:record) { create(:photo_album) }
     let(:record_url) { "/v1/photo_albums/#{record.id}/photos" }
-    let(:record_permission) { 'photo_album.read' }
+    let(:record_permission) { 'photo.read' }
 
     it_behaves_like 'a permissible model'
   end
