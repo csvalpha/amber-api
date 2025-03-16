5bfd9faa22f72887b36e8613f17962fd11b5aed0
Lodewiges <jamestreeg@gmail.com>
2025-03-16 16:01:21 +0100

    fixed all test except 1

--- spec/requests/v1/photos_controller/get_related_resources_spec.rb
+++ spec/requests/v1/photos_controller/get_related_resources_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe V1::PhotosController do
   describe 'GET /photo-albums/:id/photos', version: 1 do
-    let(:records) { create(:photo_album) }
+    let(:record) { create(:photo_album) }
     let(:record_url) { "/v1/photo_albums/#{record.id}/photos" }
     let(:record_permission) { 'photo_album.read' }
 
-    it_behaves_like 'an indexable model'
+    it_behaves_like 'a permissible model'
   end
 end
