62308987b13fedd66b4606fecef548b6f9bfc301
lodewiges <131907615+lodewiges@users.noreply.github.com>
2025-03-25 00:28:57 +0100

    Update camo_helper.rb

--- app/helpers/camo_helper.rb
+++ app/helpers/camo_helper.rb
@@ -1,7 +1,7 @@
 module CamoHelper
   # See https://github.com/ankane/camo/blob/master/lib/camo.rb
   def camo(image_url)
-    hexdigest = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'),
+    hexdigest = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'),
                                         Rails.application.config.x.camo_key, image_url)
     encoded_image_url = image_url.unpack1('H*')
     "#{Rails.application.config.x.camo_host}/#{hexdigest}/#{encoded_image_url}"
