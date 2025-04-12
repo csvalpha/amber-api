1d28f5d208ab4e02b88d04c05727ecd6c4bd009a
lodewiges <131907615+lodewiges@users.noreply.github.com>
2025-04-12 22:42:00 +0200

    Fix Message bus change (#547)
    * try another option
    
    * Initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, { url: Rails.application.config_for(:cable)['url'] }, { pool: false }
+  config.cache_store = :redis_cache_store, { url: Rails.application.config_for(:cable)['url'] }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :sidekiq
