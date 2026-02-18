a3d0610b086dc25f400f5aeb37fe0fd615cdb9ac
Timon Boer <timonboer05@gmail.com>
2026-02-18 10:37:32 +0100

    no update_all

--- db/migrate/20260216222433_fix_genootschapen_dyslexia.rb
+++ db/migrate/20260216222433_fix_genootschapen_dyslexia.rb
@@ -1,9 +1,13 @@
 class FixGenootschapenDyslexia < ActiveRecord::Migration[7.2]
   def up
-    Activity.where(category: "genootschapen").update_all(category: "genootschappen")
+    Activity.where(category: 'genootschapen').find_each do |activity|
+      activity.update!(category: 'genootschappen')
+    end
   end
 
   def down
-    Activity.where(category: "genootschappen").update_all(category: "genootschapen")
+    Activity.where(category: 'genootschappen').find_each do |activity|
+      activity.update!(category: 'genootschapen')
+    end
   end
 end
