d898fedea46e7c82e7633d3fa69023f9e71b06b5
Lodewiges <jamestreeg@gmail.com>
2026-08-28 13:41:35 +0200

    add 2 more tests

---
+++ spec/resources/v1/alumni_contribution_resource_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe V1::AlumniContributionResource, type: :resource do
+  let(:user) { create(:user) }
+  let(:context) { { user: } }
+
+  describe '#creatable_fields' do
+    it do
+      expect(described_class.creatable_fields(context)).to match_array(%i[
+                                                                         sponsoring_amount
+                                                                         help_digtus
+                                                                         help_kring
+                                                                         help_vereniging
+                                                                         help_anders
+                                                                       ])
+    end
+  end
+
+  describe '#updatable_fields' do
+    it do
+      expect(described_class.updatable_fields(context)).to match_array(%i[
+                                                                         sponsoring_amount
+                                                                         help_digtus
+                                                                         help_kring
+                                                                         help_vereniging
+                                                                         help_anders
+                                                                       ])
+    end
+  end
+end
